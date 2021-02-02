777e90691e63795bebc41a63f50327a551788da1
Christa Bridges <christa@pebblepad.co.uk>
2021-02-02 12:20:16 +0000

    loading-css Readjusted some of the JSX in StorePicker to match the tutorial and imported a stylesheet.

--- React-For-Beginners-Starter-Files/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/StorePicker.js
+++ React-For-Beginners-Starter-Files/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/StorePicker.js
@@ -4,8 +4,8 @@ class StorePicker extends React.Component {
     render() {
         return (
             <React.Fragment>
+                <form className="store-selector">
                 <h2>Enter a Store</h2>
-                <form>
                 <input placeholder="Store Name..." 
                         type="text"
                         required></input>
