e1d87b87f78b421943f898205ef834260aaf195b
Christa Bridges <christa@pebblepad.co.uk>
2021-02-09 12:29:02 +0000

    stateless-functional-components Fixed the indentation in StorePicker.js.

--- React-For-Beginners-Starter-Files/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/StorePicker.js
+++ React-For-Beginners-Starter-Files/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/StorePicker.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const StorePicker = () => (
     <React.Fragment>
-                <form className="store-selector">
-                <h2>Enter a Store</h2>
-                <input placeholder="Store Name..." 
-                        type="text"
-                        required></input>
-                <button type="submit">VISIT STORE ➡</button>
-                </form>
-            </React.Fragment>
+        <form className="store-selector">
+        <h2>Enter a Store</h2>
+        <input placeholder="Store Name..." 
+                type="text"
+                required></input>
+        <button type="submit">VISIT STORE ➡</button>
+        </form>
+    </React.Fragment>
 );
 
 export default StorePicker
