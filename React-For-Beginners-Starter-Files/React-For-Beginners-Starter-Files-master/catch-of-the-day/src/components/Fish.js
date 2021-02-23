03cd24ca7e7823873814f2651d3625a5dd743c3c
Christa Bridges <christa@pebblepad.co.uk>
2021-02-23 14:29:57 +0000

    update-order-state Completed the update order state tutorial.

--- React-For-Beginners-Starter-Files/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/Fish.js
+++ React-For-Beginners-Starter-Files/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/Fish.js
@@ -2,8 +2,12 @@ import React from "react";
 import { formatPrice } from "../helpers.js";
 
 class Fish extends React.Component {
+    handleClick = () => {
+        this.props.addToOrder(this.props.index);
+    }
     render() {
         const {image, name, price, desc, status} = this.props.details;
+        const isAvailable = status === "available";
         return (
             <li className="menu-fish">
                 <img src={image} alt={name}/>
@@ -12,7 +16,7 @@ class Fish extends React.Component {
                     <span className="price">{formatPrice(price)}</span>
                 </h3>
                 <p>{desc}</p>
-                <button>Add to cart</button>
+                <button onClick={this.handleClick} disabled={!isAvailable}>{isAvailable ? "Add To Order": "Sold Out"}</button>
             </li>
         )
     }
--- React-For-Beginners-Starter-Files/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/Inventory.js
+++ React-For-Beginners-Starter-Files/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/Inventory.js
@@ -3,7 +3,6 @@ import AddFishForm from "./AddFishForm.js";
 
 class Inventory extends React.Component {
     render() {
-        console.log(this.props);
         return (
             <div className="inventory">
                 <h2>Inventory</h2>
