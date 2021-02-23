8f2f688949112e10fb0dab4ae1a56b14a8487780
Christa Bridges <christa@pebblepad.co.uk>
2021-02-23 11:55:57 +0000

    understanding-state Completed the understanding state tutorial

--- React-For-Beginners-Starter-Files/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/App.js
+++ React-For-Beginners-Starter-Files/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/App.js
@@ -4,12 +4,26 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 
 class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {}
+    }
+
+    addFish = fish => {
+        // Copy existing state
+        const fishes = { ...this.state.fishes };
+        fishes[`fish${Date.now()}`] = fish;
+        this.setState({
+            fishes
+        });
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <Header tagline="Fresh Seafood Market"></Header>
                 <Order></Order>
-                <Inventory></Inventory>
+                <Inventory addFish={this.addFish}></Inventory>
             </div>
         )
     }
