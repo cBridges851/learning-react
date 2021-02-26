968dfd52e3d168be1c688e67fbbb36d7ee09bffd
Christa Bridges <christa@pebblepad.co.uk>
2021-02-26 15:44:47 +0000

    display-order-state Completed the tutorial, currently trying to figure out why the order is not displaying 'no longer available' if the fish becomes unavailable.

--- React-For-Beginners-Starter-Files/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/Order.js
+++ React-For-Beginners-Starter-Files/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/Order.js
@@ -2,18 +2,17 @@ import React from "react";
 import { formatPrice } from "../helpers";
 
 class Order extends React.Component {
-    renderOrder = (key) => {
+    renderOrder = key => {
         const fish = this.props.fishes[key];
         const count = this.props.order[key];
         const isAvailable = fish.status === "available";
-        console.log(isAvailable)
 
         if (!isAvailable) {
             return <li>Sorry {fish ? fish.name : "fish"} is not available</li>;
         }
 
         return (
-            <li>
+            <li key={key}>
             {count} lbs {fish.name}
 
             {formatPrice(count * fish.price)}
@@ -23,26 +22,28 @@ class Order extends React.Component {
     render() {
         const orderIds = Object.keys(this.props.order);
         const total = orderIds.reduce((prevTotal, key) => {
-        const fish = this.props.fishes[key];
-        const count = this.props.order[key];
-        const isAvailable = fish && fish.status === "available";
-        
-        if (isAvailable) {
-            return prevTotal + count * fish.price;
-        }
-        
-        return prevTotal;
+            const fish = this.props.fishes[key];
+            const count = this.props.order[key];
+            const isAvailable = fish && fish.status === "available";
+            
+            if (isAvailable) {
+                return prevTotal + (count * fish.price);
+            }
+            
+            return prevTotal;
     }, 0);
 
         return (
-        <div className="order-wrap">
-            <h2>Order</h2>
-            <ul className="order">{orderIds.map(this.renderOrder)}</ul>
-            <div className="total">
-                Total:
-                <strong>{formatPrice(total)}</strong>
+            <div className="order-wrap">
+                <h2>Order</h2>
+                <ul className="order">
+                    {orderIds.map(this.renderOrder)}
+                </ul>
+                <div className="total">
+                    Total:
+                    <strong>{formatPrice(total)}</strong>
+                </div>
             </div>
-        </div>
         );
     }
 }
