8beb95298c7ecf11e7b6d5e9cdd4fe68afbb939a
Christa Bridges <christa@pebblepad.co.uk>
2021-03-02 08:51:08 +0000

    local-storage Completed the persisting order state with local storage tutorial.

--- React-For-Beginners-Starter-Files/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/App.js
+++ React-For-Beginners-Starter-Files/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/App.js
@@ -14,10 +14,19 @@ class App extends React.Component {
 
     componentDidMount() {
         const { params } = this.props.match;
+        const localStorageRef = localStorage.getItem(params.storeId);
+        if (localStorageRef) {
+            this.setState({ order: JSON.parse(localStorageRef) });
+        }
         this.ref = base.syncState(`${params.storeId}/fishes`, {
             context: this,
             state: "fishes"
         });
+        
+    }
+
+    componentDidUpdate() {
+        localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order));
     }
 
     componentWillUnmount() {
